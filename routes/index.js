bae461b97093b54240a923718e4be16138907b96
rezpanda <nachosemu@outlook.com>
2016-06-02 12:16:54 +0200

    Tema 20. Gestión de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,41 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index');
+  res.render('index');
 });
 
-/* GET author page. */
+/* GET autor */
 router.get('/author', function(req, res, next) {
-	res.render('author');
+  res.render('author');
 });
 
-//Autoload de rutas que usen :quizId
-router.param('quizId', quizController.load); //autoload :quizId
+// Autoload de parámetros
+router.param('quizId', quizController.load);  // autoload :quizId
+router.param('userId', userController.load);  // autoload :userId
 
+// Definición de rutas de cuenta
+router.get('/users',                    userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign in
+router.post('/users',                   userController.create);  // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);    // editar información de cuenta
+router.put('/users/:userId(\\d+)',      userController.update);  // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   userController.destroy); // borrar cuenta
 
-// Definición de rutas de quizzes
-router.get('/quizzes.:format?', 				quizController.index);
-router.get('/quizzes/:quizId(\\d+).:format?', 	quizController.show);
+// Definición de rutas de /quizzes
+router.get('/quizzes.:format?',            		quizController.index);
+router.get('/quizzes/:quizId(\\d+).:format?',  	quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', 		quizController.check);
-router.get('/quizzes/new',						quizController.new);
-router.post('/quizzes', 						quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  	quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+router.get('/quizzes/new',                 		quizController.new);
+router.post('/quizzes',                    		quizController.create); 
+router.get('/quizzes/:quizId(\\d+)/edit',  		quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       		quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    		quizController.destroy);
+
 router.get('/quizzes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',     commentController.create);
 
