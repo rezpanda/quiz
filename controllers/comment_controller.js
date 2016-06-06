8f52fa85f7d8730b6a0627ff6449e72b74ae2aaf
rezpanda <nachosemu@outlook.com>
2016-06-06 18:26:14 +0200

    T24. MCV comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,42 +1,54 @@
-
 var models = require('../models');
 var Sequelize = require('sequelize');
 
 
-// GET /quizzes/:quizId/comments/new
-exports.new = function(req, res, next) {
-  var comment = models.Comment.build({text: ""});
-
-  res.render('comments/new', { comment: comment, 
-                               quiz: req.quiz
-                             });
+// Autoload el comentario asociado a :commentId
+exports.load = function(req, res, next, commentId) {
+  models.Comment.findById(commentId).then(function(comment) {
+    if(comment) {
+      req.comment = comment;
+      next();
+    } else {
+      next(new Error('No existe commentId=' + commentId));
+    }
+  }).catch(function(error) {
+    next(error);
+  });
 };
 
+// GET /quizzes/:quizId/comments/new
+exports.new = function(req, res, next) {
+  var comment = models.Comment.build({text: ''});
+  res.render('comments/new', {comment: comment, quiz: req.quiz});
+};
 
-// POST /quizes/:quizId/comments
+// POST /quizzes/:quizId/comments
 exports.create = function(req, res, next) {
-  var comment = models.Comment.build(
-      { text:   req.body.comment.text,          
-        QuizId: req.quiz.id
-      });
+  var comment = models.Comment.build({text: req.body.comment.text, QuizId: req.quiz.id});
+  comment.save().then(function(comment) {
+    req.flash('success', 'Comentario creado con éxito.');
+    res.redirect('/quizzes/' + req.quiz.id);
+  }).catch(Sequelize.ValidationError, function(error) {
+    req.flash('error', 'Errores en el formulario.');
+    for(var i in error.errors) {
+      req.flash('error', error.errors[i].value);
+    };
+    res.render('comments/new', {comment: comment, quiz: req.quiz});
+  }).catch(function(error) {
+    req.flash('error', 'Error al crear un comentario: ' + error.message);
+    next(error);
+  });
+};
 
-  comment.save()
-    .then(function(comment) {
-      req.flash('success', 'Comentario creado con éxito.');
-      res.redirect('/quizzes/' + req.quiz.id);
-    }) 
-    .catch(Sequelize.ValidationError, function(error) {
+// PUT /quizzes/:quizId/comments/:commentId/accept
+exports.accept = function(req, res, next) {
+  req.comment.accepted = true;
 
-      req.flash('error', 'Errores en el formulario:');
-      for (var i in error.errors) {
-          req.flash('error', error.errors[i].value);
-      };
-
-      res.render('comments/new', { comment: comment,
-                                   quiz:    req.quiz});
-    })
-    .catch(function(error) {
-      req.flash('error', 'Error al crear un Comentario: '+error.message);
-      next(error);
-    });    
+  req.comment.save(['accepted']).then(function(comment) {
+    req.flash('success', 'Comentario acceptado con éxito.');
+    res.redirect('/quizzes/' + req.params.quizId);
+  }).catch(function(error) {
+    req.flash('error', 'Error al aceptar un comentario: ' + error.message);
+    next(error);
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -8,44 +8,46 @@ var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+	res.render('index');
 });
 
-/* GET autor */
+/* GET author page. */
 router.get('/author', function(req, res, next) {
-  res.render('author');
+	res.render('author');
 });
 
-// Autoload de parámetros
-router.param('quizId', quizController.load);  // autoload :quizId
-router.param('userId', userController.load);  // autoload :userId
+/* Autoload de parámetros */
+router.param('quizId', quizController.load);	// Autoload :quizId 
+router.param('userId', userController.load);	// Autoload :userId
+router.param('commentId', commentController.load);	// Autoload :commentId
 
+/* Definición de rutas de quizzes */
+router.get('/quizzes.:format?', quizController.index);
+router.get('/quizzes/:quizId(\\d+).:format?', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizzes', sessionController.loginRequired, quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
-// Definición de rutas de sesión
-router.get('/session',    sessionController.new);     // formulario login
-router.post('/session',   sessionController.create);  // crear sesión
-router.delete('/session', sessionController.destroy); // destruir sesión
+/* Definición de rutas de comments */
+router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired, commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, commentController.accept);
 
-// Definición de rutas de cuenta
-router.get('/users',                    userController.index);   // listado usuarios
-router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
-router.get('/users/new',                userController.new);     // formulario sign in
-router.post('/users',                   userController.create);  // registrar usuario
-router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit);    // editar información de cuenta
-router.put('/users/:userId(\\d+)',      sessionController.loginRequired, userController.update);  // actualizar información de cuenta
-router.delete('/users/:userId(\\d+)',   sessionController.loginRequired, userController.destroy); // borrar cuenta
+/* Definición de rutas de usuarios */
+router.get('/users', userController.index);	// Listado de usuarios
+router.get('/users/:userId(\\d+)', userController.show);	// Ver un usuario
+router.get('/users/new', userController.new);	// Formulario sign-up
+router.post('/users', userController.create);	// Registrar usuario
+router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit);	// Editar cuenta
+router.put('/users/:userId(\\d+)', sessionController.loginRequired, userController.update);	// Actualizar cuenta
+router.delete('/users/:userId(\\d+)', sessionController.loginRequired, userController.destroy);	// Borrar cuenta
 
-// Definición de rutas de /quizzes
-router.get('/quizzes.:format?',            		quizController.index);
-router.get('/quizzes/:quizId(\\d+).:format?',  	quizController.show);
-router.get('/quizzes/:quizId(\\d+)/check', 		quizController.check);
-router.get('/quizzes/new',                 		sessionController.loginRequired, quizController.new);
-router.post('/quizzes',                    		sessionController.loginRequired, quizController.create); 
-router.get('/quizzes/:quizId(\\d+)/edit',  		sessionController.loginRequired, quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       		sessionController.loginRequired, quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    		sessionController.loginRequired, quizController.destroy);
-
-router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, commentController.create);
+/* Definición de rutas de sesión */
+router.get('/session', sessionController.new);	// Formulario login
+router.post('/session', sessionController.create);	// Crear sesión
+router.delete('/session', sessionController.destroy);	// Destruir sesión
 
 module.exports = router;
